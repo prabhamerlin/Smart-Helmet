38b36f4a04dcd0665ecc41bf6afcb6186bdfeb89
Prabha Merlin Alex <prabhamerlin@gmail.com>
2015-06-27 20:04:30 +0530

    Create db_to_webpage1.php

---
+++ db_to_webpage1.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+	<title>Actulus home</title>
+
+
+</head>
+<body background="safety.jpg">
+<h2 style="color:#ff00ff">Actulus Securitas Analyser</h2>
+   <form action="hi.php" method="post">
+<input type="submit" value="submit" onclick="hi.php">
+</form>
+</body>
